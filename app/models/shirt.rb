02827bedc48015136e88985fdea51d0f23b92f2a
Adarsh Pandit <adarsh@thoughtbot.com>
2012-08-19 21:55:58 -0700

    Add validations for Shirts

--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,4 +1,11 @@
 class Shirt < ActiveRecord::Base
   attr_accessible :description, :small_quantity, :medium_quantity,
     :large_quantity, :extra_large_quantity, :extra_extra_large_quantity
+
+  validates :description, presence: true
+  validates :small_quantity, presence: true
+  validates :medium_quantity, presence: true
+  validates :large_quantity, presence: true
+  validates :extra_large_quantity, presence: true
+  validates :extra_extra_large_quantity, presence: true
 end
---
+++ spec/models/shirt_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Shirt do
+  it { should allow_mass_assignment_of(:description) }
+  it { should allow_mass_assignment_of(:small_quantity) }
+  it { should allow_mass_assignment_of(:medium_quantity) }
+  it { should allow_mass_assignment_of(:large_quantity) }
+  it { should allow_mass_assignment_of(:extra_large_quantity) }
+  it { should allow_mass_assignment_of(:extra_extra_large_quantity) }
+
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:small_quantity) }
+  it { should validate_presence_of(:medium_quantity) }
+  it { should validate_presence_of(:large_quantity) }
+  it { should validate_presence_of(:extra_large_quantity) }
+  it { should validate_presence_of(:extra_extra_large_quantity) }
+end
